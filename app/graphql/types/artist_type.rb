65e5665aca60081be037b7632ddf2ad599ff9a90
Nick Spencer <strangebrew202@gmail.com>
2023-11-29 08:29:36 -0700

    Add: artists field to query type to allow query for all artists, items field to artists_type to allow calling the association of items on artists in a query

--- app/graphql/types/artist_type.rb
+++ app/graphql/types/artist_type.rb
@@ -6,6 +6,7 @@ module Types
     field :first_name, String, null: true
     field :last_name, String, null: true
     field :email, String, null: true
+    field :items, [Types::ItemType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,15 @@
 module Types
   class QueryType < Types::BaseObject
     field :items, [Types::ItemType], null: false, description: "Return a list of items"
+    field :artists, [Types::ArtistType], null:false, description: "Return a list of Artists"
 
     def items
       Item.all
     end
 
+    def artists
+      Artist.all
+    end
     # If you're using Relay, you can include the following lines
     # field :node, field: GraphQL::Types::Relay::NodeField, null: false
     # field :nodes, field: GraphQL::Types::Relay::NodesField, null: false
